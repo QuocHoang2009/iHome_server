0841cada4058dcc8a9919df4d979bf5ff1bcb579
Quoc Hoang <quoc.hoang200899@gmail.com>
2023-05-19 23:07:20 +0700

    delete helmet

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import helmet from 'helmet';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 import multer from 'multer';
@@ -45,8 +44,8 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+// app.use(helmet());
+// app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
